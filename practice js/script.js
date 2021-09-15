bd19b62bfe47fcfee6f52e714913452f1df03d0f
abinesh8900 <abineshranganathan@gmail.com>
2021-09-16 01:55:44 +0530

    this

--- practice js/script.js
+++ practice js/script.js
@@ -23,6 +23,26 @@ document.querySelector("#child").addEventListener("click", function(){
 console.log(this);
 function calcAge(birthYear) {
   console.log(2021 - birthYear);
-  console.log(this);
+  console.log(this); // this key word is undefined in stict mode
 }
 calcAge(2000);
+
+const calcAgeArr = (birthYear) => {
+  console.log(2021 - birthYear);
+  console.log(this);
+};
+calcAgeArr(2001);
+
+const jonas = {
+  year: 2002,
+  calcAge: function () {
+    console.log(2021 - this.year); // take a owner of the function which use this key word
+  },
+};
+jonas.calcAge();
+
+const matilda = {
+  year: 2005,
+};
+matilda.calcAge = jonas.calcAge;
+matilda.calcAge();
